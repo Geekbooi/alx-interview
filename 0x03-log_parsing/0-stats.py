9fdc0790b080b3623eb29eeb4cdfc340ea6f8848
Geekbooi <yosephmekonnen@gmail.com>
2023-01-22 20:57:59 -0600

    more on interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -80,5 +80,7 @@ def run():
                 print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
         print_statistics(total_file_size, status_codes_stats)
-        if __name__ == '__main__':
-            run()
+
+
+if __name__ == '__main__':
+    run()
